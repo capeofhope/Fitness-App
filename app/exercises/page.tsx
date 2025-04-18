c196ea442446b52dccba220b1cfb32bcf2191812
Shubham Sengar <shubhamsengar2303@gmail.com>
2025-04-18 17:46:56 +0530

    change the folder format

---
+++ app/exercises/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const page = () => {
+  return (
+    <div>
+      <div>
+        <Navbar/>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default page
--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import TypingAnimation from "../components/TypingAnimation";
+import Navbar from "../../components/Navbar";
+import Footer from "@/components/Footer";
+import TypingAnimation from "../../components/TypingAnimation";
 
 const page = () => {
   return (
