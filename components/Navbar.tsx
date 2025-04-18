60d23f461048325bae09fccef3104598ef16ea9f
Mrinal <mrinalkandwal@gmail.com>
2025-04-19 00:16:22 +0530

    refactor: clean up Navbar component structure and formatting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,43 +1,42 @@
-"use client"
-import React from 'react'
-import {Link as LinkScroll} from 'react-scroll';
+"use client";
+import React from "react";
+import { Link as LinkScroll } from "react-scroll";
 
-
-
+const NavLink = ({ title }: { title: string }) => (
+  <LinkScroll
+    to={title}
+    offset={-100}
+    spy
+    smooth
+    className="cursor-pointer text-white hover:border-b-2 hover:border-[#F3AAFF] transition duration-300 ease-in-out"
+  >
+    {title}
+  </LinkScroll>
+);
 
 const Navbar = () => {
-
-
-    const NavLink = ({title}:{title:string}})=>(
-        <LinkScroll
-            to={title}
-            offset={-100}
-            spy
-            smooth
-            className='cursor-pointer text-white hover:border-b-2 hover:border-[#F3AAFF] transition duration-300 ease-in-out'
-        >
-            {title}
-        </LinkScroll>
-    );
-
   return (
-    <div className='flex justify-between items-center p-4 bg-black text-white'>
-        <div className='text-2xl font-bold w-1/2 px-3'>
-            Logo
-        </div>
-        <div className='w-1/2'>
-            <ul className='flex justify-between items-center px-6 py-4 '>  
-                <li>
-                    <NavLink title="Home"/>
-                </li>
-                <li><NavLink title="Features"/></li>
-                <li><NavLink title="Contact"/></li>
+    <div className="flex justify-between items-center p-4 bg-black text-white">
+      <div className="text-2xl font-bold w-1/2 px-3">Logo</div>
+      <div className="w-1/2">
+        <ul className="flex justify-between items-center px-6 py-4 ">
+          <li>
+            <NavLink title="Home" />
+          </li>
+          <li>
+            <NavLink title="Features" />
+          </li>
+          <li>
+            <NavLink title="Contact" />
+          </li>
 
-                <li><NavLink title="Join"/></li>
-            </ul>
-        </div>
+          <li>
+            <NavLink title="Join" />
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
